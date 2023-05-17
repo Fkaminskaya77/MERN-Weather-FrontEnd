e80356d577aa36a68efe4c8d035f4e43305ad7c2
flo777 <fkaminskaya21@gmail.com>
2023-05-17 12:12:26 -0600

    getting search bar to work in console, solving bug when buttons are pressed calls api multiple times

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,30 +1,45 @@
 import React, { useEffect,useState } from 'react';
 import Current from './Components/realtimedata.js';
 import Forecast from './Components/forecastdata.js';
+import Searchbar from './Components/searchbar.js';
 
 
 function App() {
-  const [cityname, setCityName] = useState([])
-  const URLW = `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=denver`;
+  const [weather, setWeather] = useState([])
+  const [cityname, setCityName] = useState('')
+  
   // const URLF = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=denver&days=7`;
   
   
-  function getCityName() {
+  function getWeatherData() {
+    const URLW = `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${cityname}`;
     fetch(URLW)
     .then((response) => response.json())
-    .then((data) => setCityName(data))
+    .then((data) => setWeather(data))
   }
-  useEffect(getCityName,[]);
-  console.log(cityname)
-
+  console.log(weather)
+ 
+  
+  function handleInputChange(event) {
+    setCityName(event.target.value);
+  }
+  
 
   return (
     <div className="App">
       <div className='container'>
-    <Current/>
+       </div>
+      <div className='search'>
+      <input type="text" value={cityname} onChange={handleInputChange} />
+      <button onClick={getWeatherData}>Search</button>
+      </div>
+      
+        
+    <Current />
     <Forecast/>
+    <Searchbar  />
     </div>
-    </div>
+   
    
   );
 }
---
+++ weather/src/Components/searchbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Searchbar(){
+  
+  
+  
+  
+  
+    return( 
+    <div></div>
+ );
+}
+
+
+    
+
+
+
+export default Searchbar
